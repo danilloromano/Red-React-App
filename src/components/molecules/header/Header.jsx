f996895b151b61ba42d5358f87c0f421444bc110
Caio Cobacho <caiocobacho@hotmail.com>
2020-04-14 00:07:39 -0300

    Search style added

--- src/components/molecules/header/Header.jsx
+++ src/components/molecules/header/Header.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './Header.scss';
 import { IoIosMenu } from "react-icons/io";
+import Search from '../../atoms/search/Search'
 
 class Header extends Component {
+
 
     render() {
         return (
             <div className="header">
                 <div className="header__container">
-                    <div className="header__icon"><IoIosMenu/></div>
+                    <div className="header__icon"><IoIosMenu /></div>
                     <div className="header__image"></div>
-                    <div className="header__search"></div>
+                    <div className="header__search"><Search /> </div>
                     <div className="header__login"></div>
                 </div>
             </div>
